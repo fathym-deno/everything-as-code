b44dd50e5001d3ff9529dee6573b0dec0fbac98b
mcgear <michael.gearhardt@gmail.com>
2024-01-15 11:31:45 -0700

    base url support

--- configs/oAuth.config.ts
+++ configs/oAuth.config.ts
@@ -1,10 +1,12 @@
 import { createAzureOAuth, createGitHubOAuth } from "../src/services/oAuth.ts";
 
+const baseUrl = Deno.env.get("BASE_URL")!;
+
 export const gitHubOAuth = createGitHubOAuth(["admin:org", "user:email"]);
 
 export const azureFathymOAuth = createAzureOAuth(
   "AZURE_FATHYM",
-  "http://localhost:5437/signin/callback",
+  `${baseUrl}/signin/callback`,
   [
     "openid",
   ],
@@ -12,7 +14,7 @@ export const azureFathymOAuth = createAzureOAuth(
 
 export const azureOAuth = createAzureOAuth(
   "MSAL",
-  "http://localhost:5437/signin/callback",
+  `${baseUrl}/signin/callback`,
   [
     "https://management.core.windows.net//.default",
   ],
