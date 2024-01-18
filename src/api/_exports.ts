65d0db456a487a755b85e92f1746d57ee676399d
mcgear <michael.gearhardt@gmail.com>
2024-01-18 10:47:10 -0700

    no fresh

--- src/api/_exports.ts
+++ src/api/_exports.ts
@@ -2,6 +2,5 @@ export * from "./models/_exports.ts";
 export * from "./EaCAPIJWTPayload.ts";
 export * from "./EaCAPIState.ts";
 export * from "./EaCAPIUserState.ts";
-export * from "../eac/EaCHandler.ts";
 export * from "./EaCHandlers.ts";
 export * from "./UserEaCRecord.ts";
