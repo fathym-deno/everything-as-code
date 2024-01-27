c7843b81595fcdb06b3858e760e9d12965707c08
mcgear <michael.gearhardt@gmail.com>
2024-01-26 21:42:34 -0700

    no fresh

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
-import { type Config } from 'tailwindcss';
-import * as unimportant from 'npm:tailwindcss-unimportant';
-import { buildTailwindComponentsConfigs } from '@fathym/common';
-import FathymAtomicTailwindComponents from '@fathym/atomic/tailwind.components.ts';
+import { type Config } from "tailwindcss";
+import * as unimportant from "npm:tailwindcss-unimportant";
+import { buildTailwindComponentsConfigs } from "@fathym/common";
+import FathymAtomicTailwindComponents from "@fathym/atomic/tailwind.components.ts";
 
 const tailwindComponents = [...FathymAtomicTailwindComponents];
 
@@ -9,8 +9,8 @@ await buildTailwindComponentsConfigs(tailwindComponents);
 
 export default {
   content: [
-    '{routes,islands,components}/**/*.{ts,tsx}',
-    'build/tailwind-components.config',
+    "{routes,islands,components}/**/*.{ts,tsx}",
+    "build/tailwind-components.config",
   ],
   plugins: [unimportant],
 } satisfies Config;
