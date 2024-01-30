54a8753b8095810ece5836a2168e27ca04d23fe8
mcgear <michael.gearhardt@gmail.com>
2024-01-30 00:44:55 -0700

    no fresh

--- routes/api/eac/[entLookup]/status/index.ts
+++ routes/api/eac/[entLookup]/status/index.ts
@@ -1,9 +1,10 @@
 // deno-lint-ignore-file no-explicit-any
-import { HandlerContext, Handlers } from "$fresh/server.ts";
-import { respond } from "@fathym/common";
-import { EaCAPIUserState } from "../../../../../src/api/EaCAPIUserState.ts";
-import { EaCStatus } from "../../../../../src/api/models/EaCStatus.ts";
-import { denoKv } from "../../../../../configs/deno-kv.config.ts";
+import { HandlerContext, Handlers } from '$fresh/server.ts';
+import { respond } from '@fathym/common';
+import { EaCAPIUserState } from '../../../../../src/api/EaCAPIUserState.ts';
+import { EaCStatus } from '../../../../../src/api/models/EaCStatus.ts';
+import { denoKv } from '../../../../../configs/deno-kv.config.ts';
+import { EaCStatusProcessingTypes } from '../../../../../src/api/models/EaCStatusProcessingTypes.ts';
 
 export const handler: Handlers = {
   /**
@@ -16,22 +17,37 @@ export const handler: Handlers = {
     const entLookup = ctx.state.UserEaC!.EnterpriseLookup;
 
     const statiResults = await denoKv.list<EaCStatus>({
-      prefix: ["EaC", "Status", entLookup, "ID"],
+      prefix: ['EaC', 'Status', entLookup, 'ID'],
     });
 
     const stati: EaCStatus[] = [];
 
+    const url = new URL(req.url);
+
+    const statusTypes = url.searchParams
+      .getAll('statusType')
+      ?.map((st) => Number.parseInt(st) as EaCStatusProcessingTypes);
+
     for await (const status of statiResults) {
-      stati.push(status.value!);
+      if (
+        !statusTypes ||
+        statusTypes.length === 0 ||
+        statusTypes.some((st) => st === status.value!.Processing)
+      ) {
+        stati.push(status.value!);
+      }
     }
 
-    const url = new URL(req.url);
-    
-    const take = Number.parseInt(url.searchParams.get('take') || stati.length.toString());
+    const take = Number.parseInt(
+      url.searchParams.get('take') || stati.length.toString()
+    );
 
-    const orderedStati = stati.sort(
-      (a, b) => new Date(b.StartTime).getTime() - new Date(a.StartTime).getTime()
-    ).slice(0, take);
+    const orderedStati = stati
+      .sort(
+        (a, b) =>
+          new Date(b.StartTime).getTime() - new Date(a.StartTime).getTime()
+      )
+      .slice(0, take);
 
     return respond(orderedStati);
   },
