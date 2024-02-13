182e8b5d7a70ddc7c87837aa473379614a197bb0
mcgear <michael.gearhardt@gmail.com>
2024-02-12 20:58:14 -0700

    no import maps

--- tests/workshop/bench-direct.ts
+++ tests/workshop/bench-direct.ts
@@ -1,6 +1,6 @@
 // import { awaitAllCallbacks, AzureChatOpenAI } from '../test.deps.ts';
-import * as parse from "pdf-parse";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import * as parse from "npm:pdf-parse";
+import { PDFLoader } from "npm:langchain/document_loaders/fs/pdf";
 
 const loader = new PDFLoader("./training/azure/data-explorer/azure-data-explorer.pdf", {
   splitPages: false,
