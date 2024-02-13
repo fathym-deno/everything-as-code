dd2ebc86b10c745b27183a1c070c6b8e7c656930
mcgear <michael.gearhardt@gmail.com>
2024-02-12 17:21:31 -0700

    no mopdel yet

--- tests/workshop/bench.ts
+++ tests/workshop/bench.ts
@@ -1,13 +1,13 @@
 import { awaitAllCallbacks, AzureChatOpenAI, PDFLoader } from '../test.deps.ts';
 
 Deno.test('Workshop Bench', async (t) => {
-  const model = new AzureChatOpenAI({
-    modelName: 'gpt-4',
-    temperature: 0.7,
-    // maxTokens: 1000,
-    maxRetries: 5,
-    verbose: true,
-  });
+//   const model = new AzureChatOpenAI({
+//     modelName: 'gpt-4',
+//     temperature: 0.7,
+//     // maxTokens: 1000,
+//     maxRetries: 5,
+//     verbose: true,
+//   });
 
   await t.step('Invoke Test', async () => {
     const loader = new PDFLoader("../../training/azure/data-explorer/azure-data-explorer.pdf");
