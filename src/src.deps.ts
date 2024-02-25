7f71136b1581a08417d96e6207d0ef2e97c84f8f
mcgear <michael.gearhardt@gmail.com>
2024-02-25 16:17:11 -0700

    no fresh

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,7 +1,7 @@
 export * as djwt from "https://deno.land/x/djwt@v3.0.0/mod.ts";
 export * from "https://deno.land/std@0.216.0/encoding/base64.ts";
 export * as path from "https://deno.land/std@0.216.0/path/mod.ts";
-export * from "https://deno.land/x/fathym_common@v0.0.155/mod.ts";
+export * from "https://deno.land/x/fathym_common@v0.0.156/mod.ts";
 export {
   type OpenAIBaseInput,
   type OpenAIChatInput,
--- src/utils/library.helpers.ts
+++ src/utils/library.helpers.ts
@@ -1,1 +1,6 @@
-export { loadMetaUrl as loadEverythingAsCodeMetaUrl } from 'https://deno.land/x/fathym_common@v0.0.155/src/utils/library.helpers.ts';
+export { loadMetaUrl } from 'https://deno.land/x/fathym_common@v0.0.156/src/utils/library.helpers.ts';
+
+export function loadRefArchMetaUrl(path: string): string {
+    return loadMetaUrl(import.meta.resolve, path);
+  }
+  
