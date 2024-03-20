e6eb9b975954b12243ee26e04bb9359c32272528
mcgear <michael.gearhardt@gmail.com>
2024-03-20 17:54:28 -0600

    cleanup octokit refs

---
+++ octokit.ts
@@ -0,0 +1,1 @@
+export * from "./src/services/github/octokit/_exports.ts";
---
+++ src/octokit.deps.ts
@@ -0,0 +1,7 @@
+export * from "https://esm.sh/octokit@3.1.2?dts";
+export * from "https://esm.sh/v135/@octokit/core@v5.0.2/dist-types/types.d.ts";
+export * from "https://esm.sh/@octokit/auth-app@6.0.1";
+// export { } from 'https://esm.sh/@octokit/auth-oauth-user@4.0.1';
+export * from "https://esm.sh/@octokit/auth-token@4.0.0";
+export * from "https://esm.sh/@octokit/openapi-types@19.1.0";
+export * from "https://esm.sh/@octokit/plugin-rest-endpoint-methods@10.2.0";
