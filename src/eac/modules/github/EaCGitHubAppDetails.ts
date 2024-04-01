0f0e5216be48bf82c2d2893f58a7e25d7e1b5e71
mcgear <michael.gearhardt@gmail.com>
2024-04-01 15:06:47 -0600

    model rewrok

--- src/eac/modules/github/EaCGitHubAppDetails.ts
+++ src/eac/modules/github/EaCGitHubAppDetails.ts
@@ -1,15 +1,7 @@
 import { EaCVertexDetails } from "../../EaCVertexDetails.ts";
 
 export type EaCGitHubAppDetails = {
-  AppID: string;
-
-  ClientID: string;
-
-  ClientSecret: string;
-
-  PrivateKey: string;
-
-  WebhooksSecret: string;
+  ProviderLookup: string;
 } & EaCVertexDetails;
 
 export function isEaCGitHubAppDetails(
@@ -18,15 +10,7 @@ export function isEaCGitHubAppDetails(
   const gitHubAppDetails = details as EaCGitHubAppDetails;
 
   return (
-    gitHubAppDetails.AppID !== undefined &&
-    typeof gitHubAppDetails.AppID === "string" &&
-    gitHubAppDetails.ClientID !== undefined &&
-    typeof gitHubAppDetails.ClientID === "string" &&
-    gitHubAppDetails.ClientSecret !== undefined &&
-    typeof gitHubAppDetails.ClientSecret === "string" &&
-    gitHubAppDetails.PrivateKey !== undefined &&
-    typeof gitHubAppDetails.PrivateKey === "string" &&
-    gitHubAppDetails.WebhooksSecret !== undefined &&
-    typeof gitHubAppDetails.WebhooksSecret === "string"
+    gitHubAppDetails.ProviderLookup !== undefined &&
+    typeof gitHubAppDetails.ProviderLookup === "string"
   );
 }
