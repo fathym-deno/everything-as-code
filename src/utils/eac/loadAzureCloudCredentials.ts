897396f6c3d3185169d73fdd3e30830d48f4642e
mcgear <michael.gearhardt@gmail.com>
2024-05-02 18:53:39 -0600

    ref updates

--- src/utils/eac/loadAzureCloudCredentials.ts
+++ src/utils/eac/loadAzureCloudCredentials.ts
@@ -1,32 +1,79 @@
+import { ClientSecretCredential, TokenCredential } from "npm:@azure/identity";
+import { ConfidentialClientApplication } from "npm:@azure/msal-node";
 import {
   EaCCloudAsCode,
   isEaCCloudAsCode,
 } from "../../eac/modules/clouds/EaCCloudAsCode.ts";
 import { EaCCloudAzureDetails } from "../../eac/modules/clouds/EaCCloudAzureDetails.ts";
 import { EverythingAsCodeClouds } from "../../eac/modules/clouds/EverythingAsCodeClouds.ts";
-import { ClientSecretCredential } from "npm:@azure/identity";
 import { deconstructCloudDetailsSecrets } from "./helpers.ts";
 import { EaCCloudDetails } from "../../eac/modules/clouds/EaCCloudDetails.ts";
 import { EverythingAsCode } from "../../eac/EverythingAsCode.ts";
+import { AccessToken } from "../../../../../../../AppData/Local/deno/npm/registry.npmjs.org/@azure/core-auth/1.7.2/dist/esm/index.d.ts";
+
+export async function loadAzureCredentialsForToken(
+  token: string,
+): Promise<TokenCredential> {
+  return {
+    getToken: () => {
+      return Promise.resolve({
+        token: token,
+      } as AccessToken);
+    },
+  };
+}
+
+export async function loadAzureCredentialsForIDToken(
+  idToken: string,
+  clientId: string,
+  clientSecret: string,
+  scopes: string[],
+  tenantId: string = "common",
+): Promise<TokenCredential> {
+  const cca = new ConfidentialClientApplication({
+    auth: {
+      clientId,
+      clientSecret,
+      authority: `https://login.microsoftonline.com/${tenantId}`,
+    },
+  });
+
+  const authResult = await cca.acquireTokenOnBehalfOf({
+    oboAssertion: idToken,
+    scopes: scopes,
+  });
+
+  if (authResult?.accessToken) {
+    return {
+      getToken: () => {
+        return Promise.resolve({
+          token: authResult.accessToken,
+        } as AccessToken);
+      },
+    };
+  } else {
+    throw new Error("Failed to obtain access token");
+  }
+}
 
 export async function loadAzureCloudCredentials(
   entLookup: string,
   cloudLookup: string,
   loadEac: (entLookup: string) => Promise<EverythingAsCode>,
-): Promise<ClientSecretCredential>;
+): Promise<TokenCredential>;
 
 export async function loadAzureCloudCredentials(
   eac: EverythingAsCodeClouds,
   cloudLookup: string,
-): Promise<ClientSecretCredential>;
+): Promise<TokenCredential>;
 
 export async function loadAzureCloudCredentials(
   cloud: EaCCloudAsCode,
-): Promise<ClientSecretCredential>;
+): Promise<TokenCredential>;
 
 export async function loadAzureCloudCredentials(
   cloudDetails: EaCCloudDetails,
-): Promise<ClientSecretCredential>;
+): Promise<TokenCredential>;
 
 export async function loadAzureCloudCredentials(
   cloudDetailsEaCEntLookup:
@@ -36,7 +83,7 @@ export async function loadAzureCloudCredentials(
     | string,
   cloudLookup?: string,
   loadEaC?: (entLookup: string) => Promise<EverythingAsCode>,
-): Promise<ClientSecretCredential> {
+): Promise<TokenCredential> {
   let cloud: EaCCloudAsCode;
 
   if (cloudLookup) {
@@ -57,15 +104,19 @@ export async function loadAzureCloudCredentials(
     };
   }
 
-  const details = (await deconstructCloudDetailsSecrets(
-    cloud.Details,
-  )) as EaCCloudAzureDetails;
+  if ("Token" in cloud) {
+    return loadAzureCredentialsForToken(cloud.Token as string);
+  } else {
+    const details = (await deconstructCloudDetailsSecrets(
+      cloud.Details,
+    )) as EaCCloudAzureDetails;
 
-  return new ClientSecretCredential(
-    details.TenantID,
-    details.ApplicationID,
-    details.AuthKey,
-  );
+    return new ClientSecretCredential(
+      details.TenantID,
+      details.ApplicationID,
+      details.AuthKey,
+    );
+  }
 }
 
 export function loadMainAzureCredentials(): ClientSecretCredential {
