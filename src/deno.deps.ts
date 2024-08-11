58e990ee875fd217dd7a7d69e241fe9cd26a8d88
mcgear <michael.gearhardt@gmail.com>
2024-08-11 12:54:18 -0600

    publish check

--- src/deno.deps.ts
+++ src/deno.deps.ts
@@ -1,4 +1,4 @@
-export * from "jsr:@fathym/common@0.0.205/deno";
-export { type OAuthHelpers } from "jsr:@fathym/common@0.0.205/oauth";
+export * from "jsr:@fathym/common@0.0.207/deno";
+export { type OAuthHelpers } from "jsr:@fathym/common@0.0.207/oauth";
 export * from "jsr:@deno/kv-oauth@0.11.0";
 // export * from "https://raw.githubusercontent.com/fathym-deno/deno_kv_oauth/main/mod.ts";
