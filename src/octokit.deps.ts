24afc380ece0d016f0eea8db9d007fc4301a9fbb
mcgear <michael.gearhardt@gmail.com>
2024-08-11 04:20:43 -0600

    adjusted versions

--- src/octokit.deps.ts
+++ src/octokit.deps.ts
@@ -1,7 +1,7 @@
-export * from "npm:octokit@3.1.2";
-export * from "npm:@octokit/core@5.0.2/dist-types/types.d.ts";
+export * from "npm:octokit@4.0.2";
+export * from "npm:@octokit/core@6.1.2/dist-types/types.d.ts";
 export * from "npm:@octokit/auth-app@6.0.1";
 // export { } from 'https://esm.sh/@octokit/auth-oauth-user@4.0.1';
-export * from "npm:@octokit/auth-token@4.0.0";
-export { type components } from "npm:@octokit/openapi-types@19.1.0/types.d.ts";
-export * from "npm:@octokit/plugin-rest-endpoint-methods@10.2.0";
+export * from "npm:@octokit/auth-token@5.1.1";
+export { type components } from "npm:@octokit/openapi-types@22.2.0/types.d.ts";
+export * from "npm:@octokit/plugin-rest-endpoint-methods@13.2.4";
