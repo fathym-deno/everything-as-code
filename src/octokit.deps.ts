ee300f2d1e985d8a084852f901161b67b528ba9f
mcgear <michael.gearhardt@gmail.com>
2024-08-11 04:14:46 -0600

    change type ref

--- src/octokit.deps.ts
+++ src/octokit.deps.ts
@@ -3,5 +3,5 @@ export * from "npm:@octokit/core@5.0.2/dist-types/types.d.ts";
 export * from "npm:@octokit/auth-app@6.0.1";
 // export { } from 'https://esm.sh/@octokit/auth-oauth-user@4.0.1';
 export * from "npm:@octokit/auth-token@4.0.0";
-export * from "npm:@octokit/openapi-types@19.1.0";
+export { type components } from "npm:@octokit/openapi-types@19.1.0";
 export * from "npm:@octokit/plugin-rest-endpoint-methods@10.2.0";
