797f8ac1b22ab839b1abe3078537aef99ba18e21
mcgear <michael.gearhardt@gmail.com>
2024-08-12 01:21:49 -0600

    EaCJSRDistributedFileSystem

--- src/eac/modules/dfs/.exports.ts
+++ src/eac/modules/dfs/.exports.ts
@@ -2,6 +2,7 @@ export * from "./models/.exports.ts";
 export * from "./EaCDenoKVDistributedFileSystem.ts";
 export * from "./EaCDistributedFileSystem.ts";
 export * from "./EaCESMDistributedFileSystem.ts";
+export * from "./EaCJSRDistributedFileSystem.ts";
 export * from "./EaCLocalDistributedFileSystem.ts";
 export * from "./EaCNPMDistributedFileSystem.ts";
 export * from "./EaCRemoteDistributedFileSystem.ts";
---
+++ src/eac/modules/dfs/EaCJSRDistributedFileSystem.ts
@@ -0,0 +1,24 @@
+import {
+  EaCDistributedFileSystem,
+  isEaCDistributedFileSystem,
+} from "./EaCDistributedFileSystem.ts";
+
+export type EaCJSRDistributedFileSystem = {
+  Package: string;
+
+  Version: string;
+} & EaCDistributedFileSystem<"JSR">;
+
+export function isEaCJSRDistributedFileSystem(
+  dfs: unknown,
+): dfs is EaCJSRDistributedFileSystem {
+  const x = dfs as EaCJSRDistributedFileSystem;
+
+  return (
+    isEaCDistributedFileSystem("JSR", x) &&
+    x.Package !== undefined &&
+    typeof x.Package === "string" &&
+    x.Version !== undefined &&
+    typeof x.Version === "string"
+  );
+}
