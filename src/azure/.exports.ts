3f3f1c1f7a386e3c14c4d39a6ec31d7b43165c90
mcgear <michael.gearhardt@gmail.com>
2024-08-19 14:02:41 -0600

    missing exports

--- src/azure/.exports.ts
+++ src/azure/.exports.ts
@@ -3,6 +3,10 @@
  * @module
  */
 export * from "./data-lake.ts";
+export * from "./deconstructCloudDetailsSecrets.ts";
+export * from "./eacGetSecrets.ts";
+export * from "./eacSetSecrets.ts";
 export * from "./key-vault.ts";
 export * from "./kusto.ts";
+export * from "./loadAzureCloudCredentials.ts";
 export * from "./loadResoureTypeApiVersions.ts";
