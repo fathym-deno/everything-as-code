65dde05a1e5bb00c6797ea2c3a4bdf00111d8a9a
mcgear <michael.gearhardt@gmail.com>
2024-08-19 16:26:57 -0600

    kusto isolation

--- src/azure/.exports.ts
+++ src/azure/.exports.ts
@@ -7,6 +7,6 @@ export * from "./deconstructCloudDetailsSecrets.ts";
 export * from "./eacGetSecrets.ts";
 export * from "./eacSetSecrets.ts";
 export * from "./key-vault.ts";
-export * from "./kusto.ts";
+// export * from "./kusto.ts";
 export * from "./loadAzureCloudCredentials.ts";
 export * from "./loadResoureTypeApiVersions.ts";
