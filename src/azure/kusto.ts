fa3c616499d9b381c036bd355e99628c354813fc
mcgear <michael.gearhardt@gmail.com>
2024-08-20 09:07:58 -0600

    try no kusto

--- src/azure/kusto.ts
+++ src/azure/kusto.ts
@@ -1,127 +1,129 @@
-import {
-  EaCCloudResourceAsCode,
-  EaCCloudResourceFormatDetails,
-  EaCCloudWithResources,
-  EverythingAsCodeClouds,
-  TokenCredential,
-} from "./.deps.ts";
-import {
-  Client as KustoClient,
-  KustoConnectionStringBuilder,
-} from "npm:azure-kusto-data@6.0.2";
-import { loadAzureCloudCredentials } from "./loadAzureCloudCredentials.ts";
+// import {
+//   EaCCloudResourceAsCode,
+//   EaCCloudResourceFormatDetails,
+//   EaCCloudWithResources,
+//   EverythingAsCodeClouds,
+//   TokenCredential,
+// } from "./.deps.ts";
+// import {
+//   Client as KustoClient,
+//   KustoConnectionStringBuilder,
+// } from "npm:azure-kusto-data@6.0.2";
+// import { loadAzureCloudCredentials } from "./loadAzureCloudCredentials.ts";
 
-const kustoClientCache: Record<string, KustoClient> = {};
+// const kustoClientCache: Record<string, KustoClient> = {};
 
-/**
- * Loads a Kusto client for the given cluster and region.
- *
- * @param entLookup The enterprise lookup for the Kusto cluster.
- * @param cloudLookup The cloud lookup for the Kusto cluster.
- * @param resGroupLookup The resource group lookup for the Kusto cluster.
- * @param resLookups The resource lookups for the Kusto cluster.
- * @param loadEaC The function to load Everything As Code for the Kusto cluster.
- * @param svcSuffix (optional) The service suffix for the Kusto cluster.
- * @returns A Promise that resolves to the Kusto client.
- */
-export function loadKustoClient(
-  entLookup: string,
-  cloudLookup: string,
-  resGroupLookup: string,
-  resLookups: string[],
-  loadEaC: (entLookup: string) => Promise<EverythingAsCodeClouds>,
-  svcSuffix?: string,
-): Promise<KustoClient>;
+// /**
+//  * Loads a Kusto client for the given cluster and region.
+//  *
+//  * @param entLookup The enterprise lookup for the Kusto cluster.
+//  * @param cloudLookup The cloud lookup for the Kusto cluster.
+//  * @param resGroupLookup The resource group lookup for the Kusto cluster.
+//  * @param resLookups The resource lookups for the Kusto cluster.
+//  * @param loadEaC The function to load Everything As Code for the Kusto cluster.
+//  * @param svcSuffix (optional) The service suffix for the Kusto cluster.
+//  * @returns A Promise that resolves to the Kusto client.
+//  */
+// export function loadKustoClient(
+//   entLookup: string,
+//   cloudLookup: string,
+//   resGroupLookup: string,
+//   resLookups: string[],
+//   loadEaC: (entLookup: string) => Promise<EverythingAsCodeClouds>,
+//   svcSuffix?: string,
+// ): Promise<KustoClient>;
 
-/**
- * Loads a Kusto client for the given cluster and region.
- *
- * @param cluster The Kusto cluster.
- * @param region The Kusto region.
- * @param creds The authentication credentials to use for the Kusto cluster.
- * @returns A Promise that resolves to the Kusto client.
- */
-export function loadKustoClient(
-  cluster: string,
-  region: string,
-  creds: TokenCredential,
-): Promise<KustoClient>;
+// /**
+//  * Loads a Kusto client for the given cluster and region.
+//  *
+//  * @param cluster The Kusto cluster.
+//  * @param region The Kusto region.
+//  * @param creds The authentication credentials to use for the Kusto cluster.
+//  * @returns A Promise that resolves to the Kusto client.
+//  */
+// export function loadKustoClient(
+//   cluster: string,
+//   region: string,
+//   creds: TokenCredential,
+// ): Promise<KustoClient>;
 
-/**
- * Loads a Kusto client for the given cluster and region.
- *
- * @returns A Promise that resolves to the Kusto client.
- */
-export async function loadKustoClient(
-  clusterEntLookup: string,
-  regionCloudLookup: string,
-  credsResGroupLookup: TokenCredential | string,
-  resLookups?: string[],
-  loadEaC?: (entLookup: string) => Promise<EverythingAsCodeClouds>,
-  svcSuffix?: string,
-): Promise<KustoClient> {
-  let [cluster, region, creds] = [
-    clusterEntLookup,
-    regionCloudLookup,
-    credsResGroupLookup as TokenCredential,
-  ];
+// /**
+//  * Loads a Kusto client for the given cluster and region.
+//  *
+//  * @returns A Promise that resolves to the Kusto client.
+//  */
+// export async function loadKustoClient(
+//   clusterEntLookup: string,
+//   regionCloudLookup: string,
+//   credsResGroupLookup: TokenCredential | string,
+//   resLookups?: string[],
+//   loadEaC?: (entLookup: string) => Promise<EverythingAsCodeClouds>,
+//   svcSuffix?: string,
+// ): Promise<KustoClient> {
+//   let [cluster, region, creds] = [
+//     clusterEntLookup,
+//     regionCloudLookup,
+//     credsResGroupLookup as TokenCredential,
+//   ];
 
-  let clusterConectionString = "";
+//   let clusterConectionString = "";
 
-  if (typeof credsResGroupLookup === "string") {
-    clusterConectionString =
-      `${clusterEntLookup}|${regionCloudLookup}|${credsResGroupLookup as string}|${
-        resLookups?.join("-")
-      }`;
-  } else {
-    clusterConectionString = `https://${cluster}.${region}.kusto.windows.net`;
-  }
+//   if (typeof credsResGroupLookup === "string") {
+//     clusterConectionString =
+//       `${clusterEntLookup}|${regionCloudLookup}|${credsResGroupLookup as string}|${
+//         resLookups?.join("-")
+//       }`;
+//   } else {
+//     clusterConectionString = `https://${cluster}.${region}.kusto.windows.net`;
+//   }
 
-  if (!(clusterConectionString in kustoClientCache)) {
-    if (typeof credsResGroupLookup === "string") {
-      // Received enterprise lookup and cloud lookup to consruct
-      const [entLookup, cloudLookup, resGroupLookup] = [
-        clusterEntLookup,
-        regionCloudLookup,
-        credsResGroupLookup as string,
-      ];
+//   if (!(clusterConectionString in kustoClientCache)) {
+//     if (typeof credsResGroupLookup === "string") {
+//       // Received enterprise lookup and cloud lookup to consruct
+//       const [entLookup, cloudLookup, resGroupLookup] = [
+//         clusterEntLookup,
+//         regionCloudLookup,
+//         credsResGroupLookup as string,
+//       ];
 
-      const eac = await loadEaC!(entLookup);
+//       const eac = await loadEaC!(entLookup);
 
-      creds = await loadAzureCloudCredentials(eac, cloudLookup);
+//       creds = await loadAzureCloudCredentials(eac, cloudLookup);
 
-      const cloud = eac.Clouds![cloudLookup];
+//       const cloud = eac.Clouds![cloudLookup];
 
-      const resGroup = cloud.ResourceGroups![resGroupLookup];
+//       const resGroup = cloud.ResourceGroups![resGroupLookup];
 
-      const resource = resLookups?.reduce((prev, resLookup) => {
-        const res = prev.Resources![resLookup];
+//       const resource = resLookups?.reduce((prev, resLookup) => {
+//         const res = prev.Resources![resLookup];
 
-        return res;
-      }, resGroup as EaCCloudWithResources) as EaCCloudResourceAsCode;
+//         return res;
+//       }, resGroup as EaCCloudWithResources) as EaCCloudResourceAsCode;
 
-      const resDetails = resource.Details! as EaCCloudResourceFormatDetails;
+//       const resDetails = resource.Details! as EaCCloudResourceFormatDetails;
 
-      region = resDetails.Data!.Location as string;
+//       region = resDetails.Data!.Location as string;
 
-      const shortName = resDetails.Data!.ShortName as string;
+//       const shortName = resDetails.Data!.ShortName as string;
 
-      if (!svcSuffix) {
-        svcSuffix = `-data-explorer`;
-      }
+//       if (!svcSuffix) {
+//         svcSuffix = `-data-explorer`;
+//       }
 
-      cluster = `${shortName}${svcSuffix}`;
+//       cluster = `${shortName}${svcSuffix}`;
 
-      clusterConectionString = `https://${cluster}.${region}.kusto.windows.net`;
-    }
+//       clusterConectionString = `https://${cluster}.${region}.kusto.windows.net`;
+//     }
 
-    const kcs = KustoConnectionStringBuilder.withTokenCredential(
-      clusterConectionString,
-      creds,
-    );
+//     const kcs = KustoConnectionStringBuilder.withTokenCredential(
+//       clusterConectionString,
+//       creds,
+//     );
 
-    kustoClientCache[clusterConectionString] = new KustoClient(kcs);
-  }
+//     kustoClientCache[clusterConectionString] = new KustoClient(kcs);
+//   }
 
-  return kustoClientCache[clusterConectionString];
-}
+//   return kustoClientCache[clusterConectionString];
+// }
+
+export const TEMP = "Hello";
