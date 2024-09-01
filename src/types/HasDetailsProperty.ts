e4ccb4c6b589f62f4207c5b4a8dd37d51e4d13a4
mcgear <michael.gearhardt@gmail.com>
2024-09-01 00:58:50 -0600

    fix HasDetailsProperty

--- src/types/HasDetailsProperty.ts
+++ src/types/HasDetailsProperty.ts
@@ -1,5 +1,5 @@
 import { IsObject } from "./.deps.ts";
 
-export type HasDetailsProperty<T> = T extends { Details?: infer U }
-  ? IsObject<U>
+export type HasDetailsProperty<T> = [T] extends [never] ? false
+  : T extends { Details?: infer U } ? IsObject<U>
   : false;
